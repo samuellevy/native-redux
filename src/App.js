a22b40d3892d1ed83eab57bc78fd642cdcac3baf
Samuel Levy <samuellevy.dev@gmail.com>
2019-09-03 20:06:49 -0300

    programs catching courses with functional component and using redux hooks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {SafeAreaView, View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import store from '../store';
 import CourseList from './components/CourseList';
+import ProgramList from './components/ProgramList';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <CourseList />
+      {/* <CourseList /> */}
+      <ProgramList />
     </Provider>
   );
 };
---
+++ src/components/ProgramList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {useSelector} from 'react-redux';
+
+// import { Container } from './styles';
+
+export default function ProgramList() {
+  const courses = useSelector(state => state.data); //== state=>{return state.data;}
+  return (
+    <View>
+      {courses.map((item, key) => (
+        <Text>{item}</Text>
+      ))}
+    </View>
+  );
+}
